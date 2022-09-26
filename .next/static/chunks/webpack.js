53b0e33d56b5b6d185c17420bfcc0dadb7468d87
hosseinfo <hforooghy@gmail.com>
2022-09-26 20:57:27 +0400

    bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9bf58303798d70c3"; }
+/******/ 		__webpack_require__.h = function() { return "3be1c1a3b1a1ef08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
